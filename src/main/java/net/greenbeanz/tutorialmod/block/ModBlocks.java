e7f8061fcec8652a3cc2ac207cf2951e0f871668
GREENBEANZ002 <cjpgreen@outlook.com>
2024-11-27 12:59:39 +0000

    Gem blocks

--- src/main/java/net/greenbeanz/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/greenbeanz/tutorialmod/block/ModBlocks.java
@@ -29,6 +29,8 @@ public class ModBlocks {
             () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_BLOCK).sound(SoundType.AMETHYST)));
     public static final RegistryObject<Block> RUBY_BLOCK = registerBlock("ruby_block",
             () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_BLOCK).sound(SoundType.AMETHYST)));
+    public static final RegistryObject<Block> RAW_RUBY_BLOCK = registerBlock("raw_ruby_block",
+            () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.IRON_BLOCK).sound(SoundType.AMETHYST)));
 
 
 

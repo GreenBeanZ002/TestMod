36704efecd947815beb92c737d67b8a218ec3441
GREENBEANZ002 <cjpgreen@outlook.com>
2025-03-23 21:31:47 +0000

    Uranium Block

--- src/main/java/net/greenbeanz/tutorialmod/item/ModCreativeModeTabs.java
+++ src/main/java/net/greenbeanz/tutorialmod/item/ModCreativeModeTabs.java
@@ -30,6 +30,7 @@ public class ModCreativeModeTabs {
                         pOutput.accept(ModBlocks.SAPPHIRE_BLOCK.get());
                         pOutput.accept(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
                         pOutput.accept(ModBlocks.RUBY_BLOCK.get());
+                        pOutput.accept(ModBlocks.URANIUM_BLOCK.get());
                         pOutput.accept(ModBlocks.RAW_RUBY_BLOCK.get());
                         pOutput.accept(ModBlocks.AMETHYST_BLOCK.get());
                         pOutput.accept(ModBlocks.RAW_AMETHYST_BLOCK.get());
